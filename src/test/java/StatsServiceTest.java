04cf0ff1750990d979b9d526fa6d7efc27614e1e
Artem Chernyshov <black_art@list.ru>
2020-11-11 18:22:02 +0300

    Added project files

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,81 @@
+import ru.netology.stats.StatsService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @org.junit.jupiter.api.Test
+        //проверяем соответствие общей выручки за год
+    void shouldTotalSales() {
+        StatsService service = new StatsService();
+
+        int[] salesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.calculateSales(salesByMonths);
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+        //проверяем соответствие средней выручки в месяц
+    void shouldMeanSales() {
+        StatsService service = new StatsService();
+
+        int[] salesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.calculateMeanSales(salesByMonths);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+        //проверяем соответствие месяцев с максимальной выручкой
+    void shouldMaxMonthSales() {
+        StatsService service = new StatsService();
+
+        int[] salesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.calculateMaxMonthSales(salesByMonths);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+        //проверяем соответствие месяцев с минимальной выручкой
+    void shouldMinMonthSales() {
+        StatsService service = new StatsService();
+
+        int[] salesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.calculateMinMonthSales(salesByMonths);
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+        //проверяем соответствие месяцев с продажами выше среднего
+    void shouldOverAverageSales() {
+        StatsService service = new StatsService();
+
+        int[] salesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 12;
+        int actual = service.calculateOverAverageSales(salesByMonths);
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+        //проверяем соответствие месяцев с продажами ниже среднего
+    void shouldUnderAverageSales() {
+        StatsService service = new StatsService();
+
+        int[] salesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 11;
+        int actual = service.calculateUnderAverageSales(salesByMonths);
+        assertEquals(expected, actual);
+
+    }
+
+}
